a235269e02a52b247b2bcf0aa2d44cfce61b3245
ryosuke ando <ryo@ando.link>
2022-02-14 18:46:38 +0900

    add serverless api

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,7 +1,6 @@
 import { Request, Response, Router, NextFunction } from "express";
 const router = Router();
 
-/* GET users listing. */
 router.get("/", function (req: Request, res: Response, next: NextFunction) {
   res.json({ message: "welcome to the api" });
 });
